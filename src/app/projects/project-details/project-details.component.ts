6efc121d3826defafe71feb9c9f5621ee6ec0315
Ihor Khomiak <iokhomiak@gmail.com>
2018-05-09 16:13:24 +0300

    implement project-detail component

--- src/app/projects/project-details/project-details.component.ts
+++ src/app/projects/project-details/project-details.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from '../../common/protocols';
 import { ProjectsService } from '../projects.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.css']
 })
-export class ProjectDetailsComponent implements OnInit {
+export class ProjectDetailsComponent implements OnInit, OnDestroy {
 
   private project: Project;
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute, private projectService: ProjectsService) { }
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.paramsSubscription = this.route.params.subscribe(params => {
       this.project = this.projectService.getProject(params['id']);
     });
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
