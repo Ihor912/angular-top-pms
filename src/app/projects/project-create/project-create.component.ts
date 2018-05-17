d11470b31848df0a0c9ee3e85d53aa1f64c65258
Ihor Khomiak <iokhomiak@gmail.com>
2018-05-17 07:45:58 +0300

    add taskId to project dto

--- src/app/projects/project-create/project-create.component.ts
+++ src/app/projects/project-create/project-create.component.ts
@@ -16,6 +16,7 @@ export class ProjectCreateComponent implements OnInit {
     name: '',
     type: '',
     owner: '',
+    taskId: '',
     members: Array<Member>()
   };
   constructor(public projectService: ProjectsService, public router: Router) {
